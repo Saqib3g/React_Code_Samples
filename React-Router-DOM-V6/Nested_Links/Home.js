be9b9ee2f639e9dc77e31cb3e11677ab01c179d9
Saqib Khan <117062853+Saqib3g@users.noreply.github.com>
2023-02-08 19:13:46 +0530

    Create Home.js

---
+++ React-Router-DOM-V6/Nested_Links/Home.js
@@ -0,0 +1,37 @@
+import { Link} from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+const Home = () => {
+    const [state, setState] = useState(2);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    async function getData() {
+      const response = await fetch(`https://hub.dummyapis.com/employee?noofRecords=${state}&idStarts=1001`);
+      const result = await response.json();
+      setData(result);
+    }
+    getData();
+  }, [state]);
+
+  useEffect(() => {
+    document.title = `(${state}) Employees online`;
+  }, [state]);
+  return (
+    <div>
+        <button onDoubleClick={()=>window.alert("You Double Clicked idiot!")} onClick={() => setState(state + 5)}>Click me {state}</button>
+        <textarea onPaste={()=> window.alert("Dont copy-paste noobde!")}></textarea>
+    
+      {
+        data.map((element, index) => (
+          <Link to={'/${element.id'}><div className='data' key={index}>
+            <h4>{element.firstName}</h4>
+            <h4>{element.lastName}</h4>
+            <h4>{element.email}</h4>
+          </div></Link>
+        ))
+      }
+    </div>
+  )
+}
+
+export default Home
