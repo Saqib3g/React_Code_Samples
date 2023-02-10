d03c857a4eee596ab8fb55ceb56bc790e8dec5e5
Saqib Khan <117062853+Saqib3g@users.noreply.github.com>
2023-02-10 12:05:22 +0530

    Create useFetch.js

---
+++ Custom Hook/useFetch.js
@@ -0,0 +1,17 @@
+import React, {useState,useEffect} from 'react'
+
+const useFetch = (url) => {
+    const [data, setData] = useState([]);
+  useEffect(() => {
+   async function getData() {
+    const call = await fetch(url);
+    const res = await call.json();
+    console.log(res);
+    setData(res);
+   }
+   getData();
+  },[])
+  return [data];
+}
+
+export default useFetch
